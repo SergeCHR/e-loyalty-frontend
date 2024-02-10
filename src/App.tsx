d27be2609f453dfe75d7285c5a8f388d1ba9addf
scherneha <schernneha@binariks.com>
2024-02-10 22:07:49 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Input } from "./components/ui/input";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello world!</h1>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
